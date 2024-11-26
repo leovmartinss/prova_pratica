efa13916ae400e78d2e4aa916365674b3e2a0bdb
Léo Vieira Martins <126921432+leovmartinss@users.noreply.github.com>
2024-11-26 01:44:31 -0300

    Add files via upload

---
+++ prova_pratica/src/test/java/com/prova/prova_pratica/ProvaPraticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prova.prova_pratica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaPraticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
